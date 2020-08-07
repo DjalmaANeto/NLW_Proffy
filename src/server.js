0b13b03f75fb3b63c6c6bb7ad7b2bbddd3b82dbc
Djalma de Almeida Neto <42612494+DjalmaANeto@users.noreply.github.com>
2020-08-07 20:19:11 -0300

    server created and synchronized data to page study

--- src/server.js
+++ src/server.js
@@ -1,96 +1,13 @@
-//dados 
-const proffys = [
-    {
-        name: "Diego Fernandes",
-        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "99 99999 9999",
-        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from:[720],
-        time_to:[1220] 
-    },
-    {
-        name: "Diego Fernandes",
-        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "99 99999 9999",
-        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from:[720],
-        time_to:[1220] 
-    },
-    {
-        name: "Mayk Brito",
-        avatar:"https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-        whatsapp: "99 99999 9999",
-        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from:[720],
-        time_to:[1220] 
-    },
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química"
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado"
-]
-
-//funções
-function getSubject (subjectNumber) {
-    const position = +subjectNumber -1
-    return subjects[position]
-}
-
-function pageLanding(req, res){
-    return res.render("index.html")
-}
-
-function pageStudy(req, res){
-    const filters = req.query
-    return res.render("study.html", {proffys, filters, subjects, weekdays})
-}
-
-function pageGiveClasses(req, res) {
-    // Pegar os dados inseridos
-    const data = req.query
-    // Adicionar os data a lista de proffys[]
-    //Verificar se não são data vazios
-    const isNotEmpty = Object.keys(data).length != 0
-    
-    if(isNotEmpty) {
-        data.subject = getSubject(data.subject)
-        proffys.push(data)
-        return res.redirect("/study")
-    }
-    return res.render("give-classes.html", {subjects, weekdays})
-}
-
 //servidor 
 const express = require('express')
 const server = express()
+
+//importando pages
+const {
+    pageLanding, 
+    pageStudy,
+    pageGiveClasses
+} = require('./pages')
 
 //configurar nunjucks(tamplete engine node/html)
 const nunjucks = require('nunjucks')
